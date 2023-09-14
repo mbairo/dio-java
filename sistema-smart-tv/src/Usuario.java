5e689abee41239bca7e0a4529944938e69b8c7d8
mbairo <bairo.marcos@gmail.com>
2023-09-14 18:40:48 -0300

    Segunda aula

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,31 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        System.out.println(smartTv.volume);
+
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        System.out.println("Apos alterar: " + smartTv.canal);
+
+        smartTv.mudarCalnal(55);
+        System.out.println("Apos alterar: " + smartTv.canal);
+
+
+
+        System.out.println("Tv Ligada: " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual:  " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status da Tv: " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status da Tv: " + smartTv.ligada);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        
+        int num1, num2;
+        num1 = 10;
+        num2 = 20;        
+
+        String resultado = num1 == num2 ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
     }
 }
